0b4bf57ca28dc0d25bb1f6a2442d3b4dd87d4d5b
André Silva <andre.silva@posteo.de>
2023-07-09 21:54:57 +0200

    Clean forEach parameter

--- Day-8-Form-Wave-Animation/LoginForm.jsx
+++ Day-8-Form-Wave-Animation/LoginForm.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useRef } from 'react';
-import './style.css';
+import React, { useEffect, useRef } from 'react'
+import './style.css'
 
 function LoginForm() {
-  const labelsRef = useRef([]);
+  const labelsRef = useRef([])
 
   useEffect(() => {
-    labelsRef.current.forEach((label, idx) => {
+    labelsRef.current.forEach((label) => {
       label.innerHTML = label.innerText
         .split('')
         .map(
@@ -13,26 +13,28 @@ function LoginForm() {
             `<span style="transition-delay: ${idx * 50}ms">${letter}</span>`
         )
         .join('')
-    });
-  }, []);
+    })
+  }, [])
 
   return (
     <div className="container">
       <h1>Please Login</h1>
       <div className="form-control">
         <input type="text" required />
-        <label ref={el => labelsRef.current[0] = el}>Email</label> 
+        <label ref={(el) => (labelsRef.current[0] = el)}>Email</label>
       </div>
 
       <div className="form-control">
         <input type="password" required />
-        <label ref={el => labelsRef.current[1] = el}>Password</label> 
+        <label ref={(el) => (labelsRef.current[1] = el)}>Password</label>
       </div>
 
       <button className="btn">Login</button>
-      <p className="text">Don't have an account <a href="#">Register</a></p>
+      <p className="text">
+        Don't have an account <a href="#">Register</a>
+      </p>
     </div>
-  );
+  )
 }
 
-export default LoginForm;
+export default LoginForm
