4fcebf77678b2a407a2805a0fa0e07a3b3de93d2
André Silva <andre.silva@posteo.de>
2023-08-27 22:53:39 +0200

    Add keys to gitignore

--- Day-17-Movie-App/script.js
+++ Day-17-Movie-App/script.js
@@ -1,17 +1,11 @@
+import apiKey from "./apiKeys.js"
+
 const API_URL =
-  "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=7550b615756fe130d66e4d864b1015b4"
-// 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&access_token' + encodeURIComponent('eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NTUwYjYxNTc1NmZlMTMwZDY2ZTRkODY0YjEwMTViNCIsInN1YiI6IjY0ZGQyYjhmNWFiODFhMDExYzJkNzM4MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tI3q1Ckk5p_V01IS_B5eRYem0MAUXoW9t_ncAqPB0_Q')
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NTUwYjYxNTc1NmZlMTMwZDY2ZTRkODY0YjEwMTViNCIsInN1YiI6IjY0ZGQyYjhmNWFiODFhMDExYzJkNzM4MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tI3q1Ckk5p_V01IS_B5eRYem0MAUXoW9t_ncAqPB0_Q'
-//   }
-// };
+  `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=${apiKey}`
+
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280"
 const SEARCH_API =
-  'https://api.themoviedb.org/3/search/movie?api_key=7550b615756fe130d66e4d864b1015b4&query='
-// 'https://api.themoviedb.org/3/search/movie?access_token' + encodeURIComponent('eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NTUwYjYxNTc1NmZlMTMwZDY2ZTRkODY0YjEwMTViNCIsInN1YiI6IjY0ZGQyYjhmNWFiODFhMDExYzJkNzM4MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tI3q1Ckk5p_V01IS_B5eRYem0MAUXoW9t_ncAqPB0_Q')
+  `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query="`
 
 const form = document.getElementById("form")
 const search = document.getElementById("search")
@@ -32,14 +26,16 @@ function showMovies(movies) {
 
   movies.forEach((movie) => {
     const { title, poster_path, vote_average, overview } = movie
-    
+
     const movieEL = document.createElement("div")
     movieEL.classList.add("movie")
     movieEL.innerHTML = `
 <img src="${IMG_PATH + poster_path}" alt="${title}" />
 <div class="movie-info">
   <h3>${title}</h3>
-  <span class="${getClassByVote(vote_average)}">${vote_average.toFixed(1)}</span>
+  <span class="${getClassByVote(vote_average)}">${vote_average.toFixed(
+      1
+    )}</span>
 </div>
 <div class="overview">
   <h3>Overview</h3>
@@ -63,7 +59,7 @@ function getClassByVote(vote) {
 form.addEventListener("submit", (e) => {
   e.preventDefault()
   const searchTerm = search.value
-  
+
   if (searchTerm && searchTerm !== "") {
     getMovies(SEARCH_API + searchTerm)
     search.value = ""
@@ -71,5 +67,4 @@ form.addEventListener("submit", (e) => {
     window.location.reload()
   }
   console.log("searchTerm", SEARCH_API + searchTerm)
-
 })
