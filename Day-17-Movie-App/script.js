23052f17161ef3a171b21e7573fcd92876c8d6cc
André Silva <andre.silva@posteo.de>
2023-08-27 22:05:20 +0200

    Fetch data and search results

--- Day-17-Movie-App/script.js
+++ Day-17-Movie-App/script.js
@@ -1,41 +1,40 @@
 const API_URL =
-  "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=6223976c4480b3ecc1b4510be6017cb2"
+  "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=7550b615756fe130d66e4d864b1015b4"
+// 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&access_token' + encodeURIComponent('eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NTUwYjYxNTc1NmZlMTMwZDY2ZTRkODY0YjEwMTViNCIsInN1YiI6IjY0ZGQyYjhmNWFiODFhMDExYzJkNzM4MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tI3q1Ckk5p_V01IS_B5eRYem0MAUXoW9t_ncAqPB0_Q')
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NTUwYjYxNTc1NmZlMTMwZDY2ZTRkODY0YjEwMTViNCIsInN1YiI6IjY0ZGQyYjhmNWFiODFhMDExYzJkNzM4MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tI3q1Ckk5p_V01IS_B5eRYem0MAUXoW9t_ncAqPB0_Q'
+//   }
+// };
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280"
-
 const SEARCH_API =
-  "https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1"
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MjIzOTc2YzQ0ODBiM2VjYzFiNDUxMGJlNjAxN2NiMiIsInN1YiI6IjY0ZGQyYjhmNWFiODFhMDExYzJkNzM4MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qqMUIPKA1b_G90U04RQKLgojzebhq2qlV_uzWm4M0pw",
-  },
-}
+  'https://api.themoviedb.org/3/search/movie?api_key=7550b615756fe130d66e4d864b1015b4&query="'
+// 'https://api.themoviedb.org/3/search/movie?access_token' + encodeURIComponent('eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NTUwYjYxNTc1NmZlMTMwZDY2ZTRkODY0YjEwMTViNCIsInN1YiI6IjY0ZGQyYjhmNWFiODFhMDExYzJkNzM4MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tI3q1Ckk5p_V01IS_B5eRYem0MAUXoW9t_ncAqPB0_Q')
 
 const form = document.getElementById("form")
 const search = document.getElementById("search")
 
+// get initial movies
+
 getMovies(API_URL)
 
 async function getMovies(url) {
-  try {
-    const response = await fetch(url, options)
-    const data = await response.json()
-  } catch {
-    console.log("error:" + err)
-  }
-
-  form.addEventListener("submit", (e) => {
-    e.eventDefault()
-    const searchTerm = search.value
-    console.log("searchTerm", searchTerm)
-
-    if (searchTerm && searchTerm !== "") {
-      getMovies(SEARCH_API + searchTerm)
-      search.value = ""
-    } else {
-      window.location.reload()
-    }
-  })
+  const res = await fetch(url)
+  const data = await res.json()
+  console.log("json data", data)
 }
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const searchTerm = search.value
+  console.log("searchTerm", searchTerm)
+
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_API + searchTerm)
+    search.value = ""
+  } else {
+    window.location.reload()
+  }
+})
