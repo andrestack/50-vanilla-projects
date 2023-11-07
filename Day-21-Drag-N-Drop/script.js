da8767a3e864a2ca179fb626c035c9f28f27b05d
André Silva <andre.silva@posteo.de>
2023-11-07 21:20:12 +0100

    Implemented drag and drop functionality with handlers

--- Day-21-Drag-N-Drop/script.js
+++ Day-21-Drag-N-Drop/script.js
@@ -4,27 +4,42 @@ const empties = document.querySelectorAll('.empty');
 fill.addEventListener('dragstart', dragStart);
 fill.addEventListener('dragend', dragEnd);
 
+for(const empty of empties){
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
 
+}
 
 function dragStart(){
     console.log("drag start");
+    this.className += ' hold';
+    setTimeout(() => this.className = 'invisible', 0);
 }
 
 function dragEnd(){
     console.log("drag end");
+    this.className = 'fill';
 }
 
-function dragOver(){
+function dragOver(e){
     console.log("drag over");
+    e.preventDefault();
 }
-function dragEnter(){
+function dragEnter(e){
     console.log("drag enter");
+    e.preventDefault();
+    this.className += ' hovered';
 }
 
 function dragLeave(){
     console.log("drag leave");
+    this.className = 'empty';
 }
 
 function dragDrop(){
     console.log("drag drop");
+    this.className = 'empty';
+    this.append(fill);
 }
