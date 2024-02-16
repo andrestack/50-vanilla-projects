afd5abd551b340092f639d6615f9dd00e743bdca
André Silva <andre.silva@posteo.de>
2024-02-16 11:30:06 +1000

    Add documentation and copy to clipboard functionality

--- Day-31-Password-Generator/script.js
+++ Day-31-Password-Generator/script.js
@@ -14,6 +14,31 @@ const randomFunc = {
   symbol: getRandomSymbol,
 };
 
+clipboardEl.addEventListener("click", () => {
+  //A new textarea element is created in the DOM. This is a temporary element used to hold the text that will be copied to the clipboard.
+  const textarea = document.createElement("textarea");
+
+  //The text content of an element referred to as resultEl is retrieved and stored in the password variable.
+  const password = resultEl.innerText;
+  //If password is empty (i.e., if resultEl contains no text), the function returns immediately, and no further actions are taken.
+  if (!password) {
+    return;
+  }
+
+  //If password is not empty, its value is assigned to the textarea element's value property.
+  textarea.value = password;
+  //he textarea element is appended to the body of the document. This is necessary because the execCommand("copy") method can only copy the contents of elements that are currently in the document.
+  document.body.appendChild(textarea);
+  //The select() method is called on the textarea element, which selects all its text.
+  textarea.select();
+  //The execCommand("copy") method is called, which copies the selected text (i.e., the contents of the textarea element) to the clipboard.
+  document.execCommand("copy");
+  //The textarea element is removed from the document. Since its purpose was only to hold the text temporarily for copying, it's no longer needed after the text has been copied.
+  textarea.remove();
+  
+  alert("password copied to the clipboard");
+});
+
 generateEl.addEventListener("click", () => {
   const length = +lengthEl.value;
   const hasLower = lowercaseEl.checked;
@@ -33,14 +58,13 @@ generateEl.addEventListener("click", () => {
 function generatePassword(lower, upper, number, symbol, length) {
   let generatedPassword = "";
 
-  //lets you know the total of settings checked, they are the parameters of the generate function we are calling above 
+  //lets you know the total of settings checked, they are the parameters of the generate function we are calling above
   const typesCount = lower + upper + number + symbol;
 
   //filters and shows only true/checked values of the types Array
   const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
     (item) => Object.values(item)[0]
   );
-
 
   if (typesCount === 0) {
     return "";
@@ -49,9 +73,9 @@ function generatePassword(lower, upper, number, symbol, length) {
   //loops through whatever the length in the length input field and through the randomFunc object to generate a random password
   for (let i = 0; i < length; i += typesCount) {
     typesArr.forEach((type) => {
-     //lists the checked types that will be used to  generate the passowrd
-        const funcName = Object.keys(type)[0];
-   
+      //lists the checked types that will be used to  generate the passowrd
+      const funcName = Object.keys(type)[0];
+
       // the generated password will be the output of each of the Random Functions matching the types "lower", "upper" etc that is being loop here
       generatedPassword += randomFunc[funcName]();
     });
@@ -59,7 +83,7 @@ function generatePassword(lower, upper, number, symbol, length) {
 
   const finalPasswoard = generatedPassword.slice(0, length);
 
-  return finalPasswoard
+  return finalPasswoard;
 }
 
 function getRandomLower() {
